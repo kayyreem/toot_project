ab04847d32688f66dd2e9326584e7c0c70232d02
katleho <katlehosibz@gmail.com>
2024-05-10 12:05:02 +0200

    Update: experimenting with event handlers

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector ("h1");
 myHeading.textContent = "Amplifying the Commuter Voice";
+
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me!");
+  });
