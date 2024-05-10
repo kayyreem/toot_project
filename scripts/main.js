aece0fc26a52fea3e43e74b66502d274199c00a3
katleho <katlehosibz@gmail.com>
2024-05-10 12:08:31 +0200

    Update: experimenting with (click) handlers

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,5 @@ const myHeading = document.querySelector ("h1");
 myHeading.textContent = "Amplifying the Commuter Voice";
 
 document.querySelector("html").addEventListener("click", () => {
-    alert("Ouch! Stop poking me!");
+    alert("Buckle up & Let's go!");
   });
